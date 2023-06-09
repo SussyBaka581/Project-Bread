499e7f7bcdb977908f60611734a8e654c6c8738e
SussyBaka581 <clarketh@limestone.on.ca>
2023-06-09 12:28:12 -0400

    began work on combat backend

--- TLOB/Enemy.java
+++ TLOB/Enemy.java
@@ -6,10 +6,13 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class Toast extends Actor
+public class Enemy extends Actor
 {
+    public Enemy(int x, int y, int z, String p, String q){
+        
+    }
     /**
-     * Act - do whatever the Toast wants to do. This method is called whenever
+     * Act - do whatever the Enemy wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
     public void act()
--- TLOB/MyWorld.java
+++ TLOB/MyWorld.java
@@ -8,7 +8,11 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class MyWorld extends World
 {
-
+    public int health;
+    public int defense;
+    public int attack;
+    public String name;
+    public String desc;
     /**
      * Constructor for objects of class MyWorld.
      * 
@@ -17,5 +21,6 @@ public class MyWorld extends World
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1); 
+        addObject(new Enemy(health, defense, attack, name, desc), 300, 200);
     }
 }
