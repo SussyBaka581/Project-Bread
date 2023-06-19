18dd6884b7b8c2a6ea14eeeb4bd417dabcd86a85
JackySchool <132389410+JackySchool@users.noreply.github.com>
2023-06-19 19:28:06 -0400

    Add files via upload

---
+++ TLOB/Frame1.java
@@ -0,0 +1,75 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class world1 here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Frame1 extends ControlWorld
+{   
+    /**
+     * Constructor for objects of class world1.
+     * 
+     */
+    public Frame1()
+    {
+        super(1000, 800, 1);
+        if (Player.BKilled == false) {
+            addObject(new B(), 85, 700);
+        }
+        //left/right/up/down methods in ControlWorld
+        left(20, 20);
+        left(20, 105);
+        left(20, 190);
+        left(20, 275);
+        left(20, 360);
+        left(20, 445);
+        left(20, 520);
+        left(20, 605);
+        left(20, 690);
+        left(20, 775);
+        
+        down(20, 780);
+        down(105, 780);
+        down(190, 780);
+        down(275, 780);
+        down(360, 780);
+        down(445, 780);
+        down(520, 780);
+        down(605, 780);
+        down(690, 780);
+        down(775, 780);
+        down(860, 780);
+        down(945, 780);
+        down(1030, 780);
+        
+        right(980, 20);
+        right(980, 105);
+        right(980, 190);
+        right(980, 275);
+        right(980, 360);
+        right(980, 445);
+        right(980, 520);
+        right(980, 605);
+        right(980, 650);
+        
+        up(145, 20);
+        up(190, 20);
+        up(275, 20);
+        up(360, 20);
+        up(445, 20);
+        up(520, 20);
+        up(605, 20);
+        up(690, 20);
+        up(775, 20);
+        up(860, 20);
+        up(945, 20);
+        up(1030, 20);
+        
+        right(100, 0);
+        up(1000, 700);
+    }
+    
+    }
+
---
+++ TLOB/Frame2.java
@@ -0,0 +1,75 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class World2 here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Frame2 extends ControlWorld
+{
+    /**
+     * Constructor for objects of class World2.
+     * 
+     */
+    public Frame2()
+    {
+        super(1000, 800, 1);
+        
+        left(20, 20);
+        left(20, 105);
+        left(20, 190);
+        left(20, 275);
+        left(20, 360);
+        left(20, 445);
+        left(20, 520);
+        left(20, 605);
+        left(20, 655);
+        
+        down(20, 780);
+        down(105, 780);
+        down(190, 780);
+        down(275, 780);
+        down(360, 780);
+        down(445, 780);
+        down(520, 780);
+        down(605, 780);
+        down(690, 780);
+        down(775, 780);
+        down(860, 780);
+        down(945, 780);
+        down(1030, 780);
+        
+        right(980, 20);
+        right(980, 105);
+        right(980, 190);
+        right(980, 275);
+        right(980, 360);
+        right(980, 445);
+        right(980, 610);
+        //right(980, 520);
+        //right(980, 605);
+        right(980, 690);
+        right(980, 775);
+        
+        up(20, 20);
+        up(65, 20);
+        up(230, 20);
+        up(275, 20);
+        up(360, 20);
+        up(445, 20);
+        up(520, 20);
+        up(605, 20);
+        up(690, 20);
+        up(775, 20);
+        up(860, 20);
+        up(945, 20);
+        up(1030, 20);
+        
+        left(100, 0);
+        right(185, 0);
+        up(0, 700);
+        up(1000, 475);
+        down(1000, 560);
+    }
+}
